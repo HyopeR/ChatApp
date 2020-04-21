c3f8366e1869cfd7bd7699c464f9f657d3d37f01
HyopeR <tol_celik@hotmail.com>
2020-04-21 06:05:51 +0300

    socketio redis adapter

--- src/socketApi.js
+++ src/socketApi.js
@@ -6,8 +6,23 @@ const socketApi = {
     io
 };
 
+/**
+ * Redis adapter
+ */
+/*
+    Redis adapter sayesinde uygulama 2 ayrı portta çalıştırılıyor olsa bile
+    socket.io bütün portlara dataları broadcast eder.
+ */
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', socket => {
     console.log('A user logged in.');
+
+    socket.broadcast.emit('hello');
 });
 
 module.exports = socketApi;
