9fa47a308dbf5152471c7d555e1acd3ce65865e6
HyopeR <tol_celik@hotmail.com>
2020-04-22 05:53:45 +0300

    room change name

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -5,7 +5,7 @@ app.controller('chatController', ['$scope', ($scope) => {
      */
     $scope.onlineList = [];
     $scope.roomList = [];
-    $scope.activeTab = 2; // 1 Chat tab, 2 Online user tab
+    $scope.activeTab = 1; // 1 Chat tab, 2 Online user tab
 
     /**
      * Socket.io event handling.
@@ -22,8 +22,12 @@ app.controller('chatController', ['$scope', ($scope) => {
     });
 
     $scope.newRoom = () => {
-        let randomName = Math.random().toString(36).substring(7);
-        socket.emit('newRoom', randomName);
+        //let randomName = Math.random().toString(36).substring(7);
+
+        let roomName = window.prompt("Enter room name");
+        if(roomName !== '' && roomName !== null) {
+            socket.emit('newRoom', roomName);
+        }
     };
 
     $scope.changeTab = tab => {
