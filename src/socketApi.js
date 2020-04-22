c2f70969d06b0fbd4316efbcdf06e25ecb2a9fa9
HyopeR <tol_celik@hotmail.com>
2020-04-22 05:39:53 +0300

    rooms list method

--- src/socketApi.js
+++ src/socketApi.js
@@ -28,6 +28,10 @@ io.adapter(redisAdapter({
 io.on('connection', socket => {
     console.log('A user logged in with name ' + socket.request.user.name);
 
+    Rooms.list(rooms => {
+        console.log(rooms);
+    });
+
     Users.upsert(socket.id, socket.request.user);
 
     Users.list(users => {
